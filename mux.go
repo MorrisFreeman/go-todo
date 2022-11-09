5617e8f3dd2b621bb382cb5d36a91e81f1f87b12
MorrisFreeman <morrisfreeman0718@gmail.com>
2022-11-09 15:52:12 +0900

    add list_task

--- mux.go
+++ mux.go
@@ -1,12 +1,25 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/MorrisFreeman/go-todo/handler"
+	"github.com/MorrisFreeman/go-todo/store"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-playground/validator/v10"
+)
 
 func NewMux() http.Handler {
-	mux := http.NewServeMux()
+	mux := chi.NewRouter()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, _ *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		_, _ = w.Write([]byte(`{"status": "ok"}`))
 	})
+	v := validator.New()
+	at := &handler.AddTask{Store: store.Tasks, Validator: v}
+	mux.Post("/tasks", at.ServeHTTP)
+	lt := &handler.ListTask{Store: store.Tasks}
+	mux.Get("/tasks", lt.ServeHTTP)
+
 	return mux
 }
