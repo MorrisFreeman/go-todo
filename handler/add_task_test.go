22a31c5234669351b8bfeb52a2bee41d86b2e68c
MorrisFreeman <morrisfreeman0718@gmail.com>
2022-11-09 15:24:07 +0900

    add addTask

---
+++ handler/add_task_test.go
@@ -0,0 +1,65 @@
+package handler
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/MorrisFreeman/go-todo/entity"
+	"github.com/MorrisFreeman/go-todo/store"
+	"github.com/MorrisFreeman/go-todo/testutil"
+	"github.com/go-playground/validator/v10"
+)
+
+func TestAddTask(t *testing.T) {
+	t.Parallel()
+	type want struct {
+		status  int
+		rspFile string
+	}
+	tests := map[string]struct {
+		rspFile string
+		want    want
+	}{
+		"ok": {
+			rspFile: "testdata/add_task/ok_req.json.golden",
+			want: want{
+				status:  http.StatusOK,
+				rspFile: "testdata/add_task/ok_rsp.json.golden",
+			},
+		},
+		"badRequest": {
+			rspFile: "testdata/add_task/bad_req.json.golden",
+			want: want{
+				status:  http.StatusBadRequest,
+				rspFile: "testdata/add_task/bad_rsp.json.golden",
+			},
+		},
+	}
+
+	for n, tt := range tests {
+		tt := tt
+		t.Run(n, func(t *testing.T) {
+			t.Parallel()
+
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(
+				http.MethodPost,
+				"/tasks",
+				bytes.NewReader(testutil.LoadFile(t, tt.rspFile)),
+			)
+
+			sut := AddTask{
+				Store: &store.TaskStore{
+					Tasks: map[entity.TaskID]*entity.Task{},
+				},
+				Validator: validator.New(),
+			}
+			sut.ServeHTTP(w, r)
+
+			resp := w.Result()
+			testutil.AssertResponse(t, resp, tt.want.status, testutil.LoadFile(t, tt.want.rspFile))
+		})
+	}
+}
